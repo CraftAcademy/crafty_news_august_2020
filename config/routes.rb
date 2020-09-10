2d6d51c0158e91b24750ef5bdfbda7e072be60ae
emtalen <59577366+emtalen@users.noreply.github.com>
2020-09-10 11:29:19 +0200

    creating a create action for articles, adding routes and feature tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root controller: :articles, action: :index
-  
-  namespace :journalists do
-    resources :articles, only: [:show, :create]
-  end
 
-  resources :articles, only: [:show]
-end
+  resources :articles, only: [:show, :new, :create] 
+end 
---
+++ spec/features/user_can_write_an_article_spec.rb
@@ -0,0 +1,15 @@
+feature 'Visitor can write articles' do
+  before do
+    visit root_path
+    click_on 'Write Article'
+    fill_in 'Title', with: 'It is almost friday'
+    fill_in 'Content', with: 'And cheezedoodles are now available with chocolate'
+    click_on 'Create Article'
+  end
+
+  describe 'Visitor can write an article' do
+    it 'Visitor should see success message' do
+      expect(page).to have_content 'Your article was successfully created'
+    end
+  end
+end
